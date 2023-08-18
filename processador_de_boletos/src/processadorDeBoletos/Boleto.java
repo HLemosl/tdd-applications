3654f90e582963f80ccb5c8b2bda676320af262f
Henrique Lemos <henrique.leite@ccc.ufcg.edu.br>
2023-08-18 14:24:10 -0300

    Add Boleto constructor

---
+++ processador_de_boletos/src/processadorDeBoletos/Boleto.java
@@ -0,0 +1,21 @@
+package processadorDeBoletos;
+
+public class Boleto {
+
+	private String codigo;
+	private String data;
+	private double valorPago;
+	
+	Exceptions exception = new Exceptions();
+	
+	public Boleto(String codigo, String data, double valorPago) {
+		exception.verifier(codigo);
+		exception.verifier(data);
+		exception.verifier(valorPago);
+		
+		this.codigo = codigo;
+		this.data = data;
+		this.valorPago = valorPago;
+	}
+
+}
