337e1f25460b17e7e4f2590cea5248043dcd82b7
Henrique Lemos <henrique.leite@ccc.ufcg.edu.br>
2023-08-18 18:26:53 -0300

    Add verificadorDePagamento function

--- processador_de_boletos/src/processadorDeBoletos/Processador.java
+++ processador_de_boletos/src/processadorDeBoletos/Processador.java
@@ -1,17 +1,21 @@
 package processadorDeBoletos;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Processador {
 	
 	private List<Boleto> boletos;
 	private Fatura fatura;
+	private List<Pagamento> pagamentos; 
 	private Exceptions exceptions;
 	
 	public Processador() {
 		this.boletos = new ArrayList<Boleto>();
 		this.exceptions = new Exceptions();
+		this.pagamentos = new ArrayList<Pagamento>();
 	}
 
 	public void cadastraBoletos(String codigo, String data, double valorPago) {
@@ -37,6 +41,24 @@ public class Processador {
 
 	public Object getFatura() {
 		return this.fatura;
+	}
+
+	public void verificadorDePagamento(List<Boleto> boletos, Fatura fatura) {
+		double somaDosBoletos = 0.0;
+		
+		for (Boleto boleto : boletos) {
+			somaDosBoletos += boleto.getValor();
+		}
+		if (somaDosBoletos == fatura.getValor()) {
+			Date dataAtual = new Date();
+			SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+			String dataFormatada = formatoData.format(dataAtual);
+			
+			for (Boleto boleto : boletos) {
+				pagamentos.add(new Pagamento(boleto.getValor(), dataFormatada, boleto));
+			}
+			fatura.setIsPaga();
+		}
 	}
 
 }
