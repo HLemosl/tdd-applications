085c6b2850b8ff051d5c0582d888ea07c6607f57
Gabriel Fernandes <gabrielwebr@gmail.com>
2023-09-25 00:09:45 -0300

    chore: Update Functional Tests with new JUnit5 Features

--- gerenciador_de_tarefas/tests/functionalTests/EquivalencePartitionsTests.java
+++ gerenciador_de_tarefas/tests/functionalTests/EquivalencePartitionsTests.java
@@ -1,12 +1,15 @@
 package functionalTests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import gerenciadorDeTarefas.Prioridade;
 import gerenciadorDeTarefas.SistemaTarefas;
@@ -14,54 +17,36 @@ import gerenciadorDeTarefas.Tarefa;
 
 public class EquivalencePartitionsTests {
 
-	private Tarefa tarefaValida;
-	private SistemaTarefas sistemaTarefas;
+    private Tarefa tarefaValida;
+    private SistemaTarefas sistemaTarefas;
 
     @BeforeEach
     public void configurar() {
-    	tarefaValida = new Tarefa("Fazer compras", "No mercado Assaí às 09h", "12/09/2023", Prioridade.ALTA);
-    	sistemaTarefas = new SistemaTarefas();
+        tarefaValida = new Tarefa("Fazer compras", "No mercado Assaí às 09h", "12/09/2023", Prioridade.ALTA);
+        sistemaTarefas = new SistemaTarefas();
     }
 
     @Test
-    public void testTarefa() {
+    @DisplayName("Teste para adicionar tarefa válida")
+    public void testAdicionarTarefaValida() {
         sistemaTarefas.adicionarTarefa(tarefaValida);
 
         List<Tarefa> todasTarefas = sistemaTarefas.listarTodasTarefas();
-        
+
         assertEquals(1, todasTarefas.size());
-        assertThrows(IllegalArgumentException.class, () -> new Tarefa("", "No mercado Assaí às 09h", "12/09/2023", Prioridade.ALTA));
-        assertThrows(IllegalArgumentException.class, () -> new Tarefa("Fazer compras", "", "12/09/2023", Prioridade.ALTA));
-        assertThrows(IllegalArgumentException.class, () -> new Tarefa("Fazer compras", "No mercado Assaí às 09h", "", Prioridade.ALTA));
-        assertThrows(NullPointerException.class, () -> new Tarefa("Fazer compras", "No mercado Assaí às 09h", "12/09/2023", null));
     }
 
-    @Test
-    public void testAdicionarTarefa() {
-        // Casos de teste para adicionarTarefa
-    	
-    	sistemaTarefas.adicionarTarefa(tarefaValida);
-
-        List<Tarefa> todasTarefas = sistemaTarefas.listarTodasTarefas();
-        
-        assertEquals(1, todasTarefas.size()); 
-        assertThrows(IllegalArgumentException.class, () -> sistemaTarefas.adicionarTarefa(new Tarefa("", "No mercado Assaí às 09h", "12/09/2023", Prioridade.ALTA)));
-        assertThrows(IllegalArgumentException.class, () -> sistemaTarefas.adicionarTarefa(new Tarefa("Fazer compras", "", "12/09/2023", Prioridade.ALTA)));
-        assertThrows(IllegalArgumentException.class, () -> sistemaTarefas.adicionarTarefa(new Tarefa("Fazer compras", "No mercado Assaí às 09h", "", Prioridade.ALTA)));
-        assertThrows(NullPointerException.class, () -> sistemaTarefas.adicionarTarefa(new Tarefa("Fazer compras", "No mercado Assaí às 09h", "12/09/2023", null)));
-        assertThrows(NullPointerException.class, () -> sistemaTarefas.adicionarTarefa(new Tarefa(null, "No mercado Assaí às 09h", "12/09/2023", Prioridade.ALTA)));
+    @ParameterizedTest
+    @CsvSource({ "''" })
+    @DisplayName("Teste para adicionar tarefa inválida")
+    public void testAdicionarTarefaInvalida(String titulo) {
+        assertThrows(IllegalArgumentException.class, () -> sistemaTarefas.adicionarTarefa(new Tarefa(titulo, "No mercado Assaí às 09h", "12/09/2023", Prioridade.ALTA)));
     }
 
-    @Test
-    public void testAtualizarTarefa() {
-    	sistemaTarefas.adicionarTarefa(tarefaValida);
-        Tarefa tarefaAtualizada = new Tarefa("Fazer compras", "No mercado Assaí às 09h", "12/10/2023", Prioridade.ALTA);
-        sistemaTarefas.atualizarTarefa(0, tarefaAtualizada);
-
-        List<Tarefa> todasTarefas = sistemaTarefas.listarTodasTarefas();
-        
-        assertEquals("12/10/2023", todasTarefas.get(0).getDataVencimento());
-        assertThrows(IllegalArgumentException.class, () -> sistemaTarefas.atualizarTarefa(-1, tarefaAtualizada));
-        assertThrows(NullPointerException.class, () -> sistemaTarefas.atualizarTarefa(0, new Tarefa(null, "No mercado Assaí às 09h", "12/09/2023", Prioridade.ALTA)));
+    @ParameterizedTest
+    @CsvSource({ "-1" })
+    @DisplayName("Teste para atualizar tarefa com índices inválidos")
+    public void testAtualizarTarefaIndiceInvalido(int indice) {
+        assertThrows(IllegalArgumentException.class, () -> sistemaTarefas.atualizarTarefa(indice, tarefaValida));
     }
 }
